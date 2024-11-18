dc0c7bbc7383a464dafbc7f6961a8881dce84489
frndhoon <frndhoon@gmail.com>
2024-11-19 06:33:01 +0900

    feat: try, catch logic 로그인/로그아웃 기능에 추가

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -12,25 +12,35 @@ const getCsrfToken = () => {
 
 export const authServices = {
   login: async (username, password) => {
-    const response = await ky.post(`${API_URL}login/`, {
-      json: {
-        username,
-        password,
-      },
-      credentials: 'include',
-      headers: {
-        'X-CSRFToken': getCsrfToken(),
-      },
-    });
-    return response.json();
+    try {
+      const response = await ky.post(`${API_URL}login/`, {
+        json: {
+          username,
+          password,
+        },
+        credentials: 'include',
+        headers: {
+          'X-CSRFToken': getCsrfToken(),
+        },
+      });
+      return response.json();
+    } catch (error) {
+      console.error('API 요청 실패:', error);
+      throw new Error('로그인에 실패했습니다.');
+    }
   },
   logout: async () => {
-    const response = await ky.get(`${API_URL}logout/`, {
-      credentials: 'include',
-      headers: {
-        'X-CSRFToken': getCsrfToken(),
-      },
-    });
-    return response.json();
+    try {
+      const response = await ky.get(`${API_URL}logout/`, {
+        credentials: 'include',
+        headers: {
+          'X-CSRFToken': getCsrfToken(),
+        },
+      });
+      return response.json();
+    } catch (error) {
+      console.error('API 요청 실패:', error);
+      throw new Error('로그아웃에 실패했습니다.');
+    }
   },
 };
