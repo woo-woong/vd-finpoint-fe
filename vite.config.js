d1cb090b6e820360af92daf4e1fa3efe66060d7d
frndhoon <frndhoon@gmail.com>
2024-11-18 21:11:59 +0900

    chore: port number 변경 migration port와 통일

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -19,6 +22,7 @@ export default defineConfig({
       '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
       '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
       '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
+      '@services': fileURLToPath(new URL('./src/services', import.meta.url)),
     },
   },
 });
