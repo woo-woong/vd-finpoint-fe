352008d3e681eb19b0e4d2fb026eee30de0bdec7
frndhoon <frndhoon@gmail.com>
2024-11-19 15:39:32 +0900

    feat: 환율 데이터 api 요청 url 백엔드 서버로 변경

--- src/services/currencyServices.js
+++ src/services/currencyServices.js
@@ -1,18 +1,10 @@
 import ky from 'ky';
 
-// REQUEST URL -> 백엔드 서버로 변경 예정
-const KOREAXIM_REQUEST_URL = import.meta.env.VITE_KOREAXIM_API_URL;
-const KOREAXIM_API_KEY = import.meta.env.VITE_KOREAXIM_API_KEY;
+const EXCHANGE_API_URL = `${import.meta.env.VITE_BACKEND_API_URL}exchange/rates/`;
 
 export default async function currencyServices() {
   try {
-    const response = await ky.get(`${KOREAXIM_REQUEST_URL}`, {
-      searchParams: {
-        authkey: KOREAXIM_API_KEY,
-        searchdata: '2015-01-01',
-        data: 'AP01',
-      },
-    });
+    const response = await ky.get(`${EXCHANGE_API_URL}`);
     return response.json();
   } catch (error) {
     console.error('API 요청 실패:', error);
