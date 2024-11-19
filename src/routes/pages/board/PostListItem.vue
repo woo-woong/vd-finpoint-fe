0d093a3f3e37077bbc26c196b533611d378adba9
frndhoon <frndhoon@gmail.com>
2024-11-19 12:03:10 +0900

    refactor: Post List item 하나의 component로 관리

---
+++ src/routes/pages/board/PostListItem.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  id: Number,
+  title: String,
+  author: String,
+  created_at: String,
+});
+const formattedDate = computed(() => {
+  if (!props.created_at) return '';
+
+  const date = new Date(props.created_at);
+
+  return date.toLocaleString('ko-KR', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false, // 24시간 형식 사용
+  });
+});
+</script>
+
+<template>
+  <tr class="hover:bg-gray-100">
+    <td class="p-4 border-b">{{ id }}</td>
+    <td class="p-4 border-b">
+      <router-link
+        :to="`/board/${id}`"
+        class="text-blue-600 underline hover:text-blue-800"
+      >
+        {{ title }}
+      </router-link>
+    </td>
+    <td class="p-4 border-b">{{ author }}</td>
+    <td class="p-4 border-b">{{ formattedDate }}</td>
+  </tr>
+</template>
