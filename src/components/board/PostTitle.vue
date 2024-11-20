2fadae441c10c5c40598b0dbfe40f30a519dcf0b
frndhoon <frndhoon@gmail.com>
2024-11-20 23:41:21 +0900

    refactor: post 제목 ui component 추가

---
+++ src/components/board/PostTitle.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  modelValue: String,
+});
+defineEmits(['update:modelValue']);
+</script>
+
+<template>
+  <div class="mb-6">
+    <label for="title" class="block mb-2 text-sm font-semibold text-gray-700">
+      제목
+    </label>
+    <input
+      id="title"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      type="text"
+      class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+      placeholder="제목을 입력하세요"
+    />
+  </div>
+</template>
