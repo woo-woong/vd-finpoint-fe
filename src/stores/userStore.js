a80a7de4731ff4736616109d4cfa246815d59876
frndhoon <frndhoon@gmail.com>
2024-11-20 10:31:40 +0900

    fix: pinia persist 설정 새로고침 시 store 초기화 막기

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,8 +1,14 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
-export const useUserStore = defineStore('user', () => {
-  const userData = ref({});
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    const userData = ref({});
 
-  return { userData };
-});
+    return { userData };
+  },
+  {
+    persist: true,
+  }
+);
