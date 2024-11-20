a73f58ec79a1e0a32fddd24015e1baa7020cf602
frndhoon <frndhoon@gmail.com>
2024-11-20 23:40:26 +0900

    feat: 공용 확인/등록 버튼 추가

---
+++ src/components/board/ActionButtons.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  submitText: {
+    type: String,
+    required: true,
+  },
+});
+
+defineEmits(['submit', 'cancel']);
+</script>
+
+<template>
+  <div class="flex justify-end gap-2">
+    <button
+      type="button"
+      @click="$emit('submit')"
+      class="px-4 py-2 text-sm font-semibold text-white bg-blue-600 rounded-lg shadow hover:bg-blue-700"
+    >
+      {{ submitText }}
+    </button>
+    <button
+      type="button"
+      @click="$emit('cancel')"
+      class="px-4 py-2 text-sm font-semibold text-blue-600 bg-white border border-blue-600 rounded-lg shadow hover:bg-gray-100"
+    >
+      취소
+    </button>
+  </div>
+</template>
