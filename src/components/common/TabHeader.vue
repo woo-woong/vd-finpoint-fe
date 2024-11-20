1f0ddbdc83ca3e9925f34006391a88b05a8ec8f3
frndhoon <frndhoon@gmail.com>
2024-11-20 23:42:07 +0900

    refactor: tab header ui component 분리

---
+++ src/components/common/TabHeader.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <header class="w-full py-5 text-center text-white bg-blue-600 shadow-md">
+    <h1 class="text-2xl font-bold">{{ title }}</h1>
+  </header>
+</template>
