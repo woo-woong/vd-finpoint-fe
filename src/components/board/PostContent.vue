4e78c404e39fb2f1ba8cb5feb87ce4d2fc048d2c
frndhoon <frndhoon@gmail.com>
2024-11-20 23:40:57 +0900

    refactor: post 내용 ui 추가

---
+++ src/components/board/PostContent.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  modelValue: String,
+});
+defineEmits(['update:modelValue']);
+</script>
+
+<template>
+  <div class="mb-8">
+    <label for="content" class="block mb-2 text-sm font-semibold text-gray-700">
+      내용
+    </label>
+    <textarea
+      id="content"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      rows="8"
+      class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+      placeholder="내용을 입력하세요"
+    />
+  </div>
+</template>
