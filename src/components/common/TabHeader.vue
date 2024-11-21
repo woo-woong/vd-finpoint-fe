777e1bf470720b8b8cbb49ac8281b772cbe08c89
frndhoon <frndhoon@gmail.com>
2024-11-21 09:52:48 +0900

    refactor: TabHeader에 subtitle 추가

--- src/components/common/TabHeader.vue
+++ src/components/common/TabHeader.vue
@@ -4,11 +4,16 @@ defineProps({
     type: String,
     required: true,
   },
+  subtitle: {
+    type: String,
+    required: false,
+  },
 });
 </script>
 
 <template>
   <header class="w-full py-5 text-center text-white bg-blue-600 shadow-md">
     <h1 class="text-2xl font-bold">{{ title }}</h1>
+    <p class="text-sm">{{ subtitle }}</p>
   </header>
 </template>
