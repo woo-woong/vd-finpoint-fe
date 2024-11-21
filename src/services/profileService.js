634ccc0e07dad52e7869050ecad9a034f4e1eb06
frndhoon <frndhoon@gmail.com>
2024-11-21 11:32:38 +0900

    feat: 프로필 정보 불러오기 api 기능 추가

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -7,6 +7,17 @@ const API_URL = `${import.meta.env.VITE_BACKEND_API_URL}`;
 export const profileService = () => {
   const userData = useUserStore().userData;
 
+  const getProfile = async (data) => {
+    const response = await ky.get(`${API_URL}profile/`, {
+      json: data,
+      headers: {
+        'X-CSRFToken': getCsrfToken(),
+      },
+      credentials: 'include',
+    });
+    return response.json();
+  };
+
   const editProfile = async (data) => {
     const response = await ky.put(`${API_URL}profile/`, {
       json: data,
@@ -20,6 +31,7 @@ export const profileService = () => {
 
   return {
     userData,
+    getProfile,
     editProfile,
   };
 };
