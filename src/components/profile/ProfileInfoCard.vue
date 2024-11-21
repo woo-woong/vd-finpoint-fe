fdb4ca7eff65c68d4b6001878b4f87db82bea114
frndhoon <frndhoon@gmail.com>
2024-11-21 09:53:23 +0900

    refactor: 프로필 정보 profileCard component로 분리

---
+++ src/components/profile/ProfileInfoCard.vue
@@ -0,0 +1,48 @@
+<script setup>
+defineProps({
+  infoTitle: {
+    type: String,
+    required: true,
+  },
+});
+
+import { useUserStore } from '@/stores/userStore';
+
+// 사용자 정보 데이터
+const userData = useUserStore().userData;
+</script>
+
+<template>
+  <!-- 기본 정보 -->
+  <div v-if="infoTitle === '기본 정보'" class="p-4 mb-4 rounded-lg bg-gray-50">
+    <h3 class="mb-2 font-semibold text-blue-600 text-md">{{ infoTitle }}</h3>
+    <p class="py-1 text-sm text-gray-700">
+      아이디: <span class="font-semibold">{{ userData.username }}</span>
+    </p>
+    <p class="py-1 text-sm text-gray-700">
+      이름: <span class="font-semibold">{{ userData.name }}</span>
+    </p>
+    <p class="py-1 text-sm text-gray-700">
+      이메일: <span class="font-semibold">{{ userData.email }}</span>
+    </p>
+  </div>
+
+  <!-- 추가 정보 -->
+  <div v-if="infoTitle === '추가 정보'" class="p-4 rounded-lg bg-gray-50">
+    <h3 class="mb-2 font-semibold text-blue-600 text-md">{{ infoTitle }}</h3>
+    <p class="py-1 text-sm text-gray-700">
+      생년월일:
+      <span class="font-semibold">{{ userData.birth_date }}</span>
+    </p>
+    <p class="py-1 text-sm text-gray-700">
+      전화번호: <span class="font-semibold">{{ userData.phone }}</span>
+    </p>
+    <p class="py-1 text-sm text-gray-700">
+      주소: <span class="font-semibold">{{ userData.address }}</span>
+    </p>
+    <p class="py-1 text-sm text-gray-700">
+      상세주소:
+      <span class="font-semibold">{{ userData.detail_address }}</span>
+    </p>
+  </div>
+</template>
--- src/routes/pages/auth/ProfileHomePage.vue
+++ src/routes/pages/auth/ProfileHomePage.vue
@@ -1,19 +1,16 @@
 <script setup>
+import TabHeader from '@/components/common/TabHeader.vue';
+import ProfileInfoCard from '@/components/profile/ProfileInfoCard.vue';
 import UserSubscribedProducts from '@/components/profile/UserSubscribedProducts.vue';
-import { useUserStore } from '@/stores/userStore';
-
-// 사용자 정보 데이터
-const userData = useUserStore().userData;
 </script>
 
 <template>
   <div class="flex flex-col items-center w-full min-h-screen bg-gray-100">
     <!-- 헤더 -->
-    <header class="w-full py-5 text-center text-white bg-blue-600 shadow-md">
-      <h1 class="text-2xl font-bold">내 프로필</h1>
-      <p class="text-sm">나의 정보를 확인하고 관리할 수 있습니다.</p>
-    </header>
-
+    <TabHeader
+      title="내 프로필"
+      subtitle="나의 정보를 확인하고 관리할 수 있습니다."
+    />
     <!-- 프로필 콘텐츠 -->
     <main
       class="flex flex-col items-center w-full max-w-4xl px-6 py-10 mt-8 bg-white rounded-lg shadow-lg"
@@ -23,38 +20,12 @@ const userData = useUserStore().userData;
         <h2 class="mb-4 text-xl font-semibold">사용자 정보</h2>
 
         <!-- 기본 정보 -->
-        <div class="p-4 mb-4 rounded-lg bg-gray-50">
-          <h3 class="mb-2 font-semibold text-blue-600 text-md">기본 정보</h3>
-          <p class="py-1 text-sm text-gray-700">
-            아이디: <span class="font-semibold">{{ userData.username }}</span>
-          </p>
-          <p class="py-1 text-sm text-gray-700">
-            이름: <span class="font-semibold">{{ userData.name }}</span>
-          </p>
-          <p class="py-1 text-sm text-gray-700">
-            이메일: <span class="font-semibold">{{ userData.email }}</span>
-          </p>
-        </div>
+        <ProfileInfoCard infoTitle="기본 정보" />
 
         <!-- 추가 정보 -->
-        <div class="p-4 rounded-lg bg-gray-50">
-          <h3 class="mb-2 font-semibold text-blue-600 text-md">추가 정보</h3>
-          <p class="py-1 text-sm text-gray-700">
-            생년월일:
-            <span class="font-semibold">{{ userData.birth_date }}</span>
-          </p>
-          <p class="py-1 text-sm text-gray-700">
-            전화번호: <span class="font-semibold">{{ userData.phone }}</span>
-          </p>
-          <p class="py-1 text-sm text-gray-700">
-            주소: <span class="font-semibold">{{ userData.address }}</span>
-          </p>
-          <p class="py-1 text-sm text-gray-700">
-            상세주소:
-            <span class="font-semibold">{{ userData.detail_address }}</span>
-          </p>
-        </div>
+        <ProfileInfoCard infoTitle="추가 정보" />
       </div>
+
       <!-- 수정 버튼 -->
       <div class="flex justify-end w-full">
         <router-link to="/profile/edit">
