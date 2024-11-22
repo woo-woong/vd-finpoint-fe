e99dd62e327eafd4008aa479d29e15554997708a
frndhoon <frndhoon@gmail.com>
2024-11-22 21:25:03 +0900

    refactor: layout에서 SmartAssistantButton 이름 수정

--- src/routes/layouts/BaseLayout.vue
+++ src/routes/layouts/BaseLayout.vue
@@ -5,12 +5,12 @@
       class="flex flex-col items-center justify-center w-full px-4 max-w-[1200px]"
     >
       <router-view />
-      <SmartAssistantTooltip />
+      <SmartAssistantButton />
     </main>
   </div>
 </template>
 
 <script setup>
 import Header from '@/components/common/Header.vue';
-import SmartAssistantTooltip from '@/components/smartassistant/SmartAssistantButton.vue';
+import SmartAssistantButton from '@/components/smartAssistant/SmartAssistantButton.vue';
 </script>
