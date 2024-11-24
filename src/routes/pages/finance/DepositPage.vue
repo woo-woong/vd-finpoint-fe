710d578a684a642bb2a61481074783dea9368843
frndhoon <frndhoon@gmail.com>
2024-11-25 07:46:36 +0900

    feat: 예/적금 헤더 추가

--- src/routes/pages/finance/DepositPage.vue
+++ src/routes/pages/finance/DepositPage.vue
@@ -1,5 +1,6 @@
 <!-- 정기 예금 -->
 <template>
+  <TabHeader title="정기예금" subtitle="정기예금 상품을 확인할 수 있습니다." />
   <FinanceProductList
     path="deposit"
     :searchParams="{ topFinGrpNo: '020000', pageNo: '1' }"
@@ -8,4 +9,5 @@
 
 <script setup>
 import FinanceProductList from '@components/finance/FinanceProductList.vue';
+import TabHeader from '@components/common/TabHeader.vue';
 </script>
--- src/routes/pages/finance/SavingsPage.vue
+++ src/routes/pages/finance/SavingsPage.vue
@@ -1,4 +1,5 @@
 <template>
+  <TabHeader title="적금" subtitle="적금 상품을 확인할 수 있습니다." />
   <FinanceProductList
     path="savings"
     :searchParams="{ topFinGrpNo: '020000', pageNo: '1' }"
@@ -7,4 +8,5 @@
 
 <script setup>
 import FinanceProductList from '@components/finance/FinanceProductList.vue';
+import TabHeader from '@components/common/TabHeader.vue';
 </script>
