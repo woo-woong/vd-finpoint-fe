9e914ac685adda7f7bb1a2620dfb59cb9eddc96f
frndhoon <frndhoon@gmail.com>
2024-11-25 08:14:22 +0900

    style: baselayout padding 수정

--- src/routes/layouts/BaseLayout.vue
+++ src/routes/layouts/BaseLayout.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col items-center min-h-screen App">
     <Header></Header>
     <main
-      class="flex flex-col items-center justify-center w-full px-4 max-w-[1200px] pb-20"
+      class="flex flex-col items-center justify-center w-full px-4 max-w-[1200px]"
     >
       <router-view />
       <SmartAssistantButton />
