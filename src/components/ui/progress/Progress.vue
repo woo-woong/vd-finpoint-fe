0b6a73a8de8769e67d46bf7d23589270347afbbd
frndhoon <frndhoon@gmail.com>
2024-11-24 17:55:00 +0900

    style: progress bar color 파란색으로 변경

--- src/components/ui/progress/Progress.vue
+++ src/components/ui/progress/Progress.vue
@@ -25,12 +25,12 @@ const delegatedProps = computed(() => {
     :class="
       cn(
         'relative h-4 w-full overflow-hidden rounded-full bg-secondary',
-        props.class,
+        props.class
       )
     "
   >
     <ProgressIndicator
-      class="h-full w-full flex-1 bg-primary transition-all"
+      class="h-full w-full flex-1 bg-blue-500 transition-all"
       :style="`transform: translateX(-${100 - (props.modelValue ?? 0)}%);`"
     />
   </ProgressRoot>
