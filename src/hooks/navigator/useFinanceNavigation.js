4dc00b206a6514a038d4f9388519bdab07e5456c
frndhoon <frndhoon@gmail.com>
2024-11-25 17:27:28 +0900

    fix: 같은 uri로 갈 때  라우팅 시 데이터 새로고침이 안되는 현상 수정 router.push를 하지 않기 때문에 추후 수정 필요

--- src/hooks/navigator/useFinanceNavigation.js
+++ src/hooks/navigator/useFinanceNavigation.js
@@ -1,14 +1,9 @@
-import { useRouter } from 'vue-router';
-
 export function useFinanceNavigation() {
-  const router = useRouter();
-
   const navigateToFinProductDetail = (type, fin_prdt_cd) => {
     if (type && fin_prdt_cd) {
-      router.push({
-        path: `/${type.toLowerCase()}/detail/`,
-        query: { finPrdtCd: fin_prdt_cd },
-      });
+      // router 없이 페이지 이동이라 수정 필요
+      // router.push로 하면 새로고침이 안되는 현상 발생
+      window.location.href = `/${type.toLowerCase()}/detail/?finPrdtCd=${fin_prdt_cd}`;
     } else {
       console.warn('상품 정보가 부족합니다.');
     }
